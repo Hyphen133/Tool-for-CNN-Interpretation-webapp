e8010c8a0f4c5e75e68980848075ad93da68cd25
Jędrzej Hasiura <hyphen133@gmail.com>
2019-12-06 21:19:22 +0100

    Added navigation to graph

--- visualization_webapp/visualizations/views.py
+++ visualization_webapp/visualizations/views.py
@@ -1,8 +1,11 @@
-import requests
-from django.core.files import File
-from django.shortcuts import render, redirect
+import subprocess
 
+import torch
 from django.core.files.storage import default_storage
+from django.shortcuts import render, redirect
+from torch_model_loading import ModelLoader
+from visualization_core import GraphExtractor, FunctionNode
+from visualization_printing import LinkAttacher, NodeColoringTool, GraphPrinter
 
 selected_plugins = []
 file_lines = []
@@ -11,9 +14,6 @@ def index(request):
     if request.method == 'POST':
         print("Inside selection view")
         uploaded_file = request.FILES['code']
-        # print(uploaded_file.name)
-        # print(uploaded_file.size)
-        # print()
 
         for l in uploaded_file.readlines():
             file_lines.append(l)
@@ -22,11 +22,35 @@ def index(request):
         for p in plugins:
             selected_plugins.append(p)
 
-        # file_name = default_storage.save('model.py', uploaded_file)
+        file_name = default_storage.save('model.py', uploaded_file)
 
-        print(selected_plugins)
-        print(request.get_full_path)
-        return redirect('/visualizations/visualization')
+        #################   Loading model from file
+        model_loader = ModelLoader()
+        model = model_loader.load_model_from_external_file(file_name)
+        print(model)
+
+        #################   Converting model to graph
+        test_input = torch.rand(1, 3, 32, 32)
+        y = model.forward(test_input)
+        graph_extractor = GraphExtractor()
+        parent_node = FunctionNode(y.grad_fn.__class__.__name__)
+        graph_extractor.create_graph_and_associate_with_mapping(parent_node, y.grad_fn.next_functions, model, y.grad_fn)
+
+        link_attacher = LinkAttacher("www.google.com")
+        coloring_tool = NodeColoringTool("blue", "lightyellow")
+        graph_printer = GraphPrinter(link_attacher, coloring_tool)
+        dot_text = graph_printer.convert_graph_to_dot(parent_node)
+
+        with open('graph.dot', 'w') as f:
+            f.writelines(dot_text)
+
+        subprocess.check_call("dot -Tsvg graph.dot -o ./static/output_graph.svg")
+
+
+        # print(selected_plugins)
+        # print(request.get_full_path)
+        # return redirect('/visualizations/visualization')
+        return redirect('/static/output_graph.svg')
 
     plugin_names = ['feature_maps', 'gradient_maps', 'filters']
 
